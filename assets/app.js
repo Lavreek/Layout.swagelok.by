6fc320ae2964344118164a8f1e10cab7866a25c5
lavreek <lavreeksolacki@yandex.ru>
2022-11-21 17:05:12 +0300

    Sample created

---
+++ assets/app.js
@@ -0,0 +1,15 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (styles.scss in this case)
+import './styles/styles.scss';
+
+const $ = require('jquery');
+const bootstrap = require('bootstrap');
+
+// start the Stimulus application
+import './bootstrap';
